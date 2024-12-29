ddfa9116024990d591bb82f081dc23ce1bd916e3
fabian <fabian@einzelmensch.de>
2024-12-29 01:03:41 +0100

    add default components for pending, error and not found states

--- app/router.tsx
+++ app/router.tsx
@@ -4,8 +4,32 @@ import { routeTree } from './routeTree.gen'
 export function createRouter() {
   const router = createTanStackRouter({
     routeTree,
-  });
-  return router;
+    defaultPendingComponent: () => (
+      <div style={{ padding: '1rem' }}>
+        <style>
+          {`
+            @keyframes rotate {
+              from {
+                transform: rotate(0deg);
+              }
+              to {
+                transform: rotate(360deg);
+              }
+            }
+          `}
+        </style>
+        <div style={{ animation: 'infinite rotate 1s' }}>🌀</div>
+      </div>
+    ),
+    defaultErrorComponent: ({ error }) => (<ErrorComponent error={error} />),
+    defaultNotFoundComponent: () => (
+      <div>
+        <h1>Not found 🙁</h1>
+        <Link to="/">Home</Link>
+      </div>
+    ),
+  })
+  return router
 }
 
 declare module '@tanstack/react-router' {
