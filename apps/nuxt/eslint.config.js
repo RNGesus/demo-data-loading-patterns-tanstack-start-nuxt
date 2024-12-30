4619aac87ba8b7192517b69283ed96e0b43dcf1b
fabian <fabian@einzelmensch.de>
2024-12-30 18:34:39 +0100

    fix eslint config

--- apps/nuxt/eslint.config.js
+++ apps/nuxt/eslint.config.js
@@ -9,4 +9,4 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const gitignorePath = path.resolve(__dirname, '.gitignore')
 
-export default withNuxt(createConfig({}, ...includeIgnoreFile(gitignorePath)))
+export default withNuxt(createConfig({}, includeIgnoreFile(gitignorePath)))
