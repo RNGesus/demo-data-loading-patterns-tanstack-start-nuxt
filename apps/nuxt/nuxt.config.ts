06e51391ca764038ec030220dc31129bd57483aa
fabian <fabian@einzelmensch.de>
2024-12-30 11:39:44 +0100

    set nuxt compatibility version to 4

--- apps/nuxt/nuxt.config.ts
+++ apps/nuxt/nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: '2024-12-30',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint'],
+  future: {
+    compatibilityVersion: 4,
+  },
 })
