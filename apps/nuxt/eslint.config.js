158ba047ae6f6403298548d22be22f22b85d0d56
fabian <fabian@einzelmensch.de>
2024-12-30 18:29:12 +0100

    include nuxt's local gitignore in eslint config

--- apps/nuxt/eslint.config.js
+++ apps/nuxt/eslint.config.js
@@ -1,4 +1,12 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { includeIgnoreFile } from '@eslint/compat'
 import createConfig from '@project/eslint-config/create-config'
+
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(createConfig({}, {}))
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const gitignorePath = path.resolve(__dirname, '.gitignore')
+
+export default withNuxt(createConfig({}, ...includeIgnoreFile(gitignorePath)))
