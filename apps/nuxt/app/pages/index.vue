2ed5c2546a061225cf5700d8bc492d4767d69b5c
fabian <fabian@einzelmensch.de>
2024-12-30 12:14:08 +0100

    add railway station service to nuxt app

--- apps/nuxt/app/pages/index.vue
+++ apps/nuxt/app/pages/index.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+import { apiClient } from '@project/railway-station-service/client'
+
+const { data: countries } = useAsyncData(() => apiClient.getCountries())
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <pre>{{ countries && JSON.stringify(countries, null, 2) }}</pre>
   </div>
 </template>
