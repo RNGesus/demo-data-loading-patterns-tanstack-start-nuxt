c5bbbb1ab45f7c3d477e78d1adaa8a2312cb5851
fabian <fabian@einzelmensch.de>
2025-01-02 17:53:06 +0100

    add photo station output

---
+++ apps/tanstack-start/app/countries/photoStation.serverFn.ts
@@ -0,0 +1,14 @@
+import { apiClient } from '@project/railway-station-service/client'
+import { createServerFn } from '@tanstack/start'
+import { z } from 'vinxi'
+
+export const photoStationServerFn = createServerFn({ method: 'GET' })
+  .validator(z.object({ country: z.string() }))
+  // @ts-expect-error -- FIXME: there is a type error with the inferred types of the 'getPhotoStationByCountry' method
+  .handler(async ({ data }) => {
+    const photoStations = await apiClient.getPhotoStationByCountry({ params: { country: data.country } })
+    const { stations, photographers, licenses, photoBaseUrl } = photoStations
+    const stationsCount = stations.length
+    const photos = stations.flatMap(station => station.photos)
+    return { photoBaseUrl, photographers, licenses, photos, stationsCount }
+  })
