86c583fac306a98b7db8254b2e9d48d835f6e565
fabian <fabian@einzelmensch.de>
2025-01-03 17:11:27 +0100

    improve alt text of railway station images

--- apps/nuxt/app/countries/Photos.vue
+++ apps/nuxt/app/countries/Photos.vue
@@ -13,7 +13,7 @@ const route = useRoute('countries')
   <div class="photos">
     <div v-for="photo in photos" :key="photo.id" class="photo-card">
       <figure class="photo-figure">
-        <NuxtImg width="384" height="216" class="photo-img" loading="lazy" :src="photoBaseUrl + photo.path" :alt="`Photo of a train station in ${(route.params.country?.toString() ?? '').toUpperCase()} by ${photo.photographer}`" />
+        <NuxtImg width="384" height="216" class="photo-img" loading="lazy" :src="photoBaseUrl + photo.path" :alt="`Photo of a railway station in ${(route.params.country?.toString() ?? '').toUpperCase()} by ${photo.photographer}`" />
         <figcaption class="photographer-caption">
           {{ `by ${photo.photographer} at ${new Date(photo.createdAt).toLocaleDateString('en-GB')}` }}
         </figcaption>
