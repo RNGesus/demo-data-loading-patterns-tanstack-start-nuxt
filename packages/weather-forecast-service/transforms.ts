885cebbdaa6c2de0a6f8f68a4f2c701f2031fac7
fabian <fabian@einzelmensch.de>
2025-01-16 21:19:11 +0100

    clean up weather forecast service

--- packages/weather-forecast-service/transforms.ts
+++
@@ -1,8 +0,0 @@
-import type { PhotoStations } from './types'
-
-export function toStationPhotos(photoStations: PhotoStations) {
-  const { stations, photographers, licenses, photoBaseUrl } = photoStations
-  const stationsCount = stations.length
-  const photos = stations.flatMap(station => station.photos)
-  return { photoBaseUrl, stationsCount, photographers, licenses, photos }
-}
