25b411e197e7888b0eee0adaf10cd6625b724353
fabian <fabian@einzelmensch.de>
2025-01-21 19:59:13 +0100

    add energy charts power page

---
+++ apps/nuxt/app/pages/energyCharts/[country].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const route = useRoute('energyCharts-country')
+const { data } = useFetch('/api/energyCharts/power', {
+  query: { country: computed(() => route.params.country) },
+})
+</script>
+
+<template>
+  <div>{{ data }}</div>
+</template>
