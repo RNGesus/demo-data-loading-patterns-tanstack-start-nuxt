1bde6e9fc639287e5ce91b6a3bab6c8816d1fa7c
fabian <fabian@einzelmensch.de>
2025-01-21 19:58:15 +0100

    adapt Dropdown components from tanstack start app

---
+++ apps/nuxt/app/components/Dropdown/List.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+    <slot />
+  </ul>
+</template>
---
+++ apps/nuxt/app/components/Dropdown/Trigger.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div tabindex="0" role="button" class="btn m-1">
+    <slot />
+  </div>
+</template>
