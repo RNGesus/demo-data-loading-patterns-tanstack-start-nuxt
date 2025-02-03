09e03fd25f031c4d0dfc21b38d7e8d2a32224d63
fabian <fabian@einzelmensch.de>
2025-02-03 21:57:01 +0100

    improve formatters

--- packages/helpers/formatters.ts
+++ packages/helpers/formatters.ts
@@ -1,19 +1,15 @@
+const energyChartDataPointFormatter = new Intl.NumberFormat('de-DE', {
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+})
 export function formatEnergyChartDataPoint(number: number) {
-  return new Intl.NumberFormat(
-    'de-DE',
-    {
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
-    },
-  ).format(number)
+  return energyChartDataPointFormatter.format(number)
 }
 
+const energyChartDataPointDateFormatter = new Intl.DateTimeFormat('de-DE', {
+  timeStyle: 'short',
+  timeZone: 'Europe/Berlin',
+})
 export function formatEnergyChartDataPointDate(unixSeconds: number) {
-  return new Date(unixSeconds * 1000).toLocaleString(
-    'de-DE',
-    {
-      timeStyle: 'short',
-      timeZone: 'Europe/Berlin',
-    },
-  )
+  return energyChartDataPointDateFormatter.format(new Date(unixSeconds * 1000))
 }
