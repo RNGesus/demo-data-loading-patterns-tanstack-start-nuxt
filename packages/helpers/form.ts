daac496b5cc07b880d714d3fcad9bdae498f6ac2
fabian <fabian@einzelmensch.de>
2025-02-04 20:26:39 +0100

    use zod for parsing formdata contents

--- packages/helpers/form.ts
+++ packages/helpers/form.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod'
 
-const cleanedFormDataEntrySchema = z.union([
+const cleanedFormDataEntriesSchema = z.union([
   z.tuple([z.literal('page'), z.coerce.number().min(2)]),
   z.tuple([z.string().min(1), z.string().min(1)]),
-])
+]).catch(['', '']).array().transform(entries => entries.filter(([key]) => !!key))
 
 export function unwrapFormData<E extends EventTarget | null>(eventTarget: E) {
   if (!(eventTarget instanceof HTMLFormElement)) {
@@ -12,10 +12,10 @@ export function unwrapFormData<E extends EventTarget | null>(eventTarget: E) {
 
   const formData = new FormData(eventTarget)
 
-  const cleanedFormDataEntries = Array
-    .from(formData)
-    .filter(entry => cleanedFormDataEntrySchema.safeParse(entry).success)
-
-  const formDataObject = Object.fromEntries(cleanedFormDataEntries)
+  const parsedFormDataEntries = cleanedFormDataEntriesSchema.safeParse(Array.from(formData))
+  if (!parsedFormDataEntries.success) {
+    return
+  }
+  const formDataObject = Object.fromEntries(parsedFormDataEntries.data)
   return formDataObject
 }
