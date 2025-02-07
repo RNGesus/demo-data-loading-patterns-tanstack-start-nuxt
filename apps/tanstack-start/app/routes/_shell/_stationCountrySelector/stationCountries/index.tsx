0b2857b0677c9c5a7155110f99b46ff0f0c80224
fabian <fabian@einzelmensch.de>
2025-02-07 21:00:54 +0100

    update dependencies

--- apps/tanstack-start/app/routes/_shell/_stationCountrySelector/stationCountries/index.tsx
+++ apps/tanstack-start/app/routes/_shell/_stationCountrySelector/stationCountries/index.tsx
@@ -20,7 +20,7 @@ function RouteComponent() {
     <>
       <h1>Countries with apps</h1>
       <ul className="flex flex-col gap-4">
-        {countries.map((country) => (
+        {countries.map(country => (
           <li key={country.code}>
             <StationCountrySection country={country}>
               <TimeTableTemplate template={country.timetableUrlTemplate} />
