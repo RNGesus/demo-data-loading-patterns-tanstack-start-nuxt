410f22b8affe9f8cc8ecff2c8a05a6c9d474ab6c
fabian <fabian@einzelmensch.de>
2025-04-10 21:38:35 +0200

    remove unused config

--- apps/nuxt/nuxt.config.ts
+++ apps/nuxt/nuxt.config.ts
@@ -3,9 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/eslint', '@nuxt/image'],
-  imports: {
-    dirs: ['@project/vue-components'],
-  },
   devtools: {
     enabled: true,
     timeline: {
