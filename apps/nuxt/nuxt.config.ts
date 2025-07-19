4fde505a9a21931075d4126f31b3974838053373
fabian <fabian@einzelmensch.de>
2025-07-19 21:47:48 +0200

    bump nuxt compatibility date

--- apps/nuxt/nuxt.config.ts
+++ apps/nuxt/nuxt.config.ts
@@ -26,7 +26,7 @@ export default defineNuxtConfig({
     compatibilityVersion: 4,
   },
   experimental: { typedPages: true },
-  compatibilityDate: '2025-07-06',
+  compatibilityDate: '2025-07-19',
   vite: {
     plugins: [tailwindcss()],
   },
